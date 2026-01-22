56f73a8edf8dc7401d95b831efbb803fbe98ff6f
ritiksg440-creator <ritiksg440@gmail.com>
2026-01-22 19:53:13 +0545

    create First.js

---
+++ First.js
@@ -0,0 +1,42 @@
+const Base_URL="https://2024-03-06.currency-api.pages.dev/v1/currencies;
+const dropdowns=document.querySelectorAll(".dropdown select");
+const btn=document.querySelector("button");
+
+
+for (let select of dropdowns) {
+  for (currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "To" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+
+  select.addEventListener("change", (evt) =>{
+    updateFlag(evt.target);
+  });
+}
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+const updateExchangeRate =  () => {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+
+});
